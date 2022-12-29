5e7056994394d8659f71bd0702f248fe054d9e66
Divyansh <divyansh.72003@gmail.com>
2022-12-29 16:58:50 +0530

    Created Javascript for counter program

---
+++ Two_Project/app.js
@@ -0,0 +1,20 @@
+let count=0;
+let val=document.querySelector("#value");
+let btns=document.querySelectorAll(".btn");
+
+btns.forEach(function(btn){
+    btn.addEventListener("click",function(e){
+        let styles=e.currentTarget.classList;
+        if(styles.contains("decrease")){
+            count-=1;
+        }
+        if(styles.contains("increase")){
+            count+=1;
+        }
+        if(styles.contains("reset")){
+            count=0;
+        }
+        val.textContent=count;        
+    })
+        
+})
