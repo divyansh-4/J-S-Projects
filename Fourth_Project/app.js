2699a9255d65bf34b8e08080a8e0e196ff09c54c
Divyansh <divyansh.72003@gmail.com>
2022-12-30 16:08:09 +0530

    Created Javascript and HTML for NavBar project

---
+++ Fourth_Project/app.js
@@ -0,0 +1,17 @@
+// classList - shows/gets all classes
+// contains - checks classList for specific class
+// add - add class
+// remove - remove class
+// toggle - toggles class
+let togba=document.querySelector(".nav-toggle");
+let links=document.querySelector(".links");
+console.log(links)
+togba.addEventListener("click",function(){
+    // if(links.classList.contains("show-links")){
+    //     links.classList.remove("show-links");
+    // }
+    // else{
+    //     links.classList.add("show-links");
+    // }
+    links.classList.toggle("show-links");
+})
